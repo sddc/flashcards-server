a44c7a1b8ce6c534ddb33576db7766169969f10b
Sean Dela Cruz <sean.delacruz@gmail.com>
2019-04-08 16:24:47 -0700

    added routes and knex env config

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello world!'));
+const decks = require('./routes/api/decks');
+const cards = require('./routes/api/cards');
+
+app.use('/api/decks', decks);
+app.use('/api/cards', cards);
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
